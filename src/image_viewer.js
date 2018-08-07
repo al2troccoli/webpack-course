b3baf4a5875347c375194fdb8509ba9fdb8e4c3a
Aldo Troccoli <al2troccoli@gmail.com>
2018-08-08 00:01:11 +0200

    instalando loaders

---
+++ src/image_viewer.js
@@ -0,0 +1,6 @@
+import '../styles/image_viewer.css'
+
+const image = document.createElement('img');
+image.src = 'http://p-hold.com/200/300';
+
+document.body.appendChild(image);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
   }
+
+
 };
 
 module.exports = config;
