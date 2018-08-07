d45214770ebbd64d5c5585bc73cd9f6c02c57b63
Aldo Troccoli <al2troccoli@gmail.com>
2018-08-08 00:51:00 +0200

    agregando imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 const config = {
   entry: './src/index.js',
   output: {
@@ -18,6 +19,16 @@ const config = {
           loader: 'css-loader'
         }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
