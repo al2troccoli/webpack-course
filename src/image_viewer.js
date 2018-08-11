1f04d1372b849b705a7dbb99f536e6be2506e4d9
Aldo Troccoli <al2troccoli@gmail.com>
2018-08-12 00:53:23 +0200

    usand System.import

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,8 +1,19 @@
-import big from '../assets/big.jpg';
+//import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css'
 
 const image = document.createElement('img');
-image.src = 'http://p-hold.com/200/300';
-
+image.src = small;
 document.body.appendChild(image);
+
+/*
+const imageBig = document.createElement('img');
+imageBig.src = big;
+document.body.appendChild(imageBig);
+*/
+
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
+  document.body.appendChild(image);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import sum from './sum';
-import './image_viewer';
+//import sum from './sum';
+//import './image_viewer';
 
-const total = sum(10, 5);
-console.log(total);
+//const total = sum(10, 5);
+//console.log(total);
+
+/* Usando System.import */
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer.js').then(module => {
+    console.log(module);
+  })
+}
+
+document.body.appendChild(button);
